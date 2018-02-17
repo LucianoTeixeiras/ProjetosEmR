30b3b6abb0e82d90808e2ce1e69a892ff2ffc6dd
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-02-17 16:25:53 -0200

    Commi final

--- AnaliseChuvasDiasDeFinados/impor.R
+++ AnaliseChuvasDiasDeFinados/impor.R
@@ -7,3 +7,25 @@ summary(dados)
 
 str(dados)
 
+install.packages("lubridate")
+install.packages("Rcpp")
+
+library(lubridate)
+
+dados$Data = dmy(dados$Data)
+
+dados2 = dados[month(dados$Data) ==11 & day(dados$Data) == 2,c('Data','Precipitacao')]
+#View(dados2)
+
+dados2$Choveu = as.factor(dados2$Precipitacao != 0)
+
+#View(dados2)
+
+str(dados2)
+
+summary(dados2)
+
+#Gerando Grafico
+
+palette(c("blue","red"))
+plot(dados2$Data, dados2$Precipitacao, main = "Finados CHove?", xlab = "Ano", ylab = "Precipitação", col = dados2$Choveu)
