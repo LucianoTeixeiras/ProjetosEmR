4da34c2c1af5f6959332d7af83eaacf0e2c6eb9a
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-03-12 16:23:10 -0300

    AJuste script R install packages

--- CopyInstalledPackages/copyInstalledPackages.r
+++ CopyInstalledPackages/copyInstalledPackages.r
@@ -1,12 +1,29 @@
 # run on old computer / r version
-setwd("C:/Temp/") # or any other existing temp directory
-setwd("/Users/Florian/Dropbox/temp")
+setwd("~/GitHub/ProjetosEmR/CopyInstalledPackages")
 packages <- installed.packages()[,"Package"]
 save(packages, file="Rpackages")
 
 
 # run on new computer / r version
-setwd("C:/Temp/") # or any other existing temp directory
+setwd("~/GitHub/ProjetosEmR/CopyInstalledPackages")
 load("Rpackages")
 for (p in setdiff(packages, installed.packages()[,"Package"]))
 install.packages(p)
+
+# store_packages.R
+#
+# stores a list of your currently installed packages
+
+tmp = installed.packages()
+
+installedpackages = as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+save(installedpackages, file="~/GitHub/ProjetosEmR/CopyInstalledPackages/installed_packages-RAPD.rda")
+
+
+# restore_packages.R
+#
+# installs each package from the stored list of packages
+
+load("~/GitHub/ProjetosEmR/CopyInstalledPackages/installed_packages-RAPD.rda")
+
+for (count in 1:length(installedpackages)) install.packages(installedpackages[count])
