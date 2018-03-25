41670425cfa3d7cca31cb51084499e5935e12377
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-03-25 18:18:07 -0300

    Ajuste final pós separação dos pacotes

--- CopyInstalledPackages/copyInstalledPackages.r
+++ CopyInstalledPackages/copyInstalledPackages.r
@@ -9,21 +9,3 @@ setwd("~/GitHub/ProjetosEmR/CopyInstalledPackages")
 load("Rpackages")
 for (p in setdiff(packages, installed.packages()[,"Package"]))
 install.packages(p)
-
-# store_packages.R
-#
-# stores a list of your currently installed packages
-
-tmp = installed.packages()
-
-installedpackages = as.vector(tmp[is.na(tmp[,"Priority"]), 1])
-save(installedpackages, file="~/GitHub/ProjetosEmR/CopyInstalledPackages/installed_packages-RAPD.rda")
-
-
-# restore_packages.R
-#
-# installs each package from the stored list of packages
-
-load("~/GitHub/ProjetosEmR/CopyInstalledPackages/installed_packages-RAPD.rda")
-
-for (count in 1:length(installedpackages)) install.packages(installedpackages[count])
