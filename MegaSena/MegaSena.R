a17f8b1664ee63d5af9d26454af9772de40ae85f
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-06-03 23:30:25 -0300

    Commit MegaSena

---
+++ MegaSena/MegaSena.R
@@ -0,0 +1,15 @@
+#
+library(readxl)
+D_megase <- read_excel("MegaSena/D_megase.xlsx")
+head(D_megase)
+D_megase
+
+
+D_megase <- read_excel("D_megase.xlsx", col_types = c("numeric", 
+                                                      "numeric", "numeric", "numeric", "numeric", 
+                                                      "numeric", "numeric", "numeric", "skip", 
+                                                      "numeric", "skip", "skip", "skip", 
+                                                      "skip", "skip", "skip", "skip", "skip", 
+                                                      "skip", "skip", "skip"))
+
+class(D_megase)
