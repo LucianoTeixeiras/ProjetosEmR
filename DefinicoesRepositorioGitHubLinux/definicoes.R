edbc3f01daeedaf4ae0082375f1393fdbb3ef413
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-06-08 09:01:10 -0300

    Commit Final

--- DefinicoesRepositorioGitHubLinux/definicoes.R
+++ DefinicoesRepositorioGitHubLinux/definicoes.R
@@ -1,43 +1,52 @@
-#Teste de comunicação com o repositorio
+# Comunicação com o Repositório
 
-#1- Precisamos gerar uma chave SSH que seu computador vai usar pra se autorizar com o Github. Digite o seguinte comando no Git Bash:
-  
+# 1 - Gerar chave SSH.
+
+# Github
+
 ssh-keygen -t rsa -b 4096 -C "luciano.teixeiras@gmail.com"
 
-#O resultado será:
+# GitLab
+
+ssh-keygen -t rsa -C "luciano.teixeiras@gmail.com" -b 4096
+
+
+# O resultado será:
   
 # Generating public/private rsa key pair.  
 # Enter file in which to save the key (/Users/you/.ssh/id_rsa): [Press enter]  
-# Você quer salvar a chave nesse arquivo mesmo, só dê enter.
 
-Depois, ele vai pedir uma senha:
-  
-  Enter passphrase (empty for no passphrase): [Type a passphrase]  
-Enter same passphrase again: [Type passphrase again]  
-Essa senha você vai ter que digitar toda vez que for baixar algo de um repositório ou enviar algo pra lá. Eu deixo sem mesmo. 
-Se quiser deixar sem, só dê enter. Se não, coloque a senha e confirme.
+# Defina uma senha, nomralmente a mesma do GitHub e/ou GitLab
 
-Em seguida, você verá uma mensagem dizendo que deu tudo certo:
-  
-  Your identification has been saved in /Users/you/.ssh/id_rsa.  
+# Your identification has been saved in /Users/you/.ssh/id_rsa.  
 # Your public key has been saved in /Users/you/.ssh/id_rsa.pub.
 # The key fingerprint is:
 # 01:0f:f4:3b:ca:85:d6:17:a1:7d:f0:68:9d:f0:a2:db seuemail@dominio.com
-2- Agora, precisamos adicionar a chave que criamos ao ssh-agent. Primeiro, vamos ativa-lo:
-  
-  ssh-agent -s  
 
-Em seguida, vamos adicionar a chave que geramos ao ssh-agent:
+# 2 - Adicionar a chave criada ao ssh-agent.
+
+# Ativando o agnete:
+  
+ssh-agent -s
+
+# Add chave ao agente
   
 ssh-add ~/.ssh/id_rsa
 
-3- Agora, vamos associar a chave que geramos à nossa conta do Github. 
-Para copiar a chave do bash, digite o seguinte comando:
-  
-clip < ~/.ssh/id_rsa.pub
+# 3 - Associar a chave gerada à conta do Github/GitLab.
 
-#Tools/Shell
+# Copiando a chave para o cache do teclado:
+
+cat ~/.ssh/id_rsa.pub | clip
+
+#GitHub
 
 git config --global user.email "luciano.teixeiras@gmail.com"
 git config --global user.name "LucianoTeixeiras"
+git config --global credential.helper 'cache --timeout=10000000'
+
+#GitLab
+
+git config --global user.email "luciano.teixeiras@gmail.com"
+git config --global user.name "luciano.teixeiras"
 git config --global credential.helper 'cache --timeout=10000000'
