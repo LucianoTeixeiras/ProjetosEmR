35924b73cb9ef53f47e6b54eab545c9ad6382736
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-06-27 22:49:54 -0300

    commit

--- CopyInstalledPackages/copyInstalledPackages.r
+++ CopyInstalledPackages/copyInstalledPackages.r
@@ -1,15 +1,19 @@
 # run on old computer / r version
 setwd("~/GitHub/ProjetosEmR/CopyInstalledPackages")
+
 packages <- installed.packages()[,"Package"]
 
 packages
 
 save(packages, file="Packages-LTS-02")
 
-
 # run on new computer / r version
 setwd("~/GitHub/ProjetosEmR/CopyInstalledPackages")
+
 getwd()
+
 load("Packages-LTS-02")
+
 for (p in setdiff(packages, installed.packages()[,"Package"]))
+
 install.packages(p)
